9930e5d5356e93211b6694d0bf0703cd85180df5
d-willb <39524812+d-willb@users.noreply.github.com>
2018-06-13 18:38:35 +0200

    Add files via upload

---
+++ js/scripts-wb-a4p.js
@@ -0,0 +1,217 @@
+var $win          = $(window);
+var $header       = $('.site-banner');
+var $socials      = $('.socials--sticky');
+var $animate      = $('.section-head, .secteurs, .decouvrez');
+var $wrappedItems = $('.la-item-img, .at-illust, .center img');
+var $wrappedText  = $('.at-main-title, .article-wrapper h4');
+
+/*
+ * Mac
+ */
+$('body').toggleClass('is-mac', navigator.platform.match(/(Mac|iPhone|iPod|iPad)/i) ? true : false);
+
+$animate.addClass('animate');
+
+/*
+ * Containers
+ */
+$wrappedItems.wrap('<div class="container"></div>');
+
+/*
+ * Article socials
+ */
+if ($('.socials-primary').length) {
+	$('.socials-primary').appendTo('.at-content');
+}
+
+/*
+ * Texte Article full
+ */
+if ($('body').hasClass('full')) {
+	$('.article-intro').appendTo('.at-content');
+}
+
+/*
+ * Animated text count
+ */
+$('.js-count-to').each(function() {
+	var $this           = $(this);
+	var $statsContainer = $this.closest('.stats');
+	var number          = $this.data('count-to');
+
+	$win.on('load scroll', function() {
+		var winST = $win.scrollTop();
+
+		if (winST + $win.outerHeight() / 2 > $statsContainer.offset().top && !$this.hasClass('counted')) {
+			$this.addClass('counted');
+
+			$({ counter: 0 }).animate({
+				counter: number
+			}, {
+				step: function(now) {
+					if (now < 1000) {
+						$this.text(parseInt(now));
+					} else {
+						$this.text((parseInt(now) / 1000).toFixed(3));
+					}
+				},
+				duration: 2000
+			});
+		}
+	});
+});
+
+/*
+ * Languages
+ */
+$('.ls-lang-list').append('<li class="ls-lang-item ls-lang-de"><a href="#" class="ls-lang-link"></a></li><li class="ls-lang-item ls-lang-es"><a href="#" class="ls-lang-link"></a></li><li class="ls-lang-item ls-lang-it"><a href="#" class="ls-lang-link"></a></li>');
+
+/*
+ * Titres article shadow
+ */
+$wrappedText.wrapInner('<span></span>');
+
+/*
+ * Scroll to 
+ */
+$('.js-scroll')
+	.off('click')
+	.on('click', function(e) {
+		e.preventDefault();
+
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top
+		}, 700);
+	});
+
+/*
+ * Secteurs box shadow
+ */
+$('.secteurs .ql-item .txt-btn').wrapInner('<span></span>');
+
+/*
+ * Youmax plugin
+ */
+if ($('#youmax').length) {
+	$('#youmax').youmax({
+		apiKey: 'AIzaSyCNbIqgoVrq7IPkHr_NBMquEXAFu9zv474',
+		vimeoAccessToken: '',
+		clientId: '438137961980-vlefbf8sgps4r5fqon9u92m93n0hc1pi.apps.googleusercontent.com',
+		channel: '',
+		youtube_playlist_videos: [{
+			name: 'Videos',
+			url: 'https://www.youtube.com/watch?v=5Tpjlmy4wsk&list=PLZqovFqfp6OiS0aRnGZ9CKHOXNuJZnPFD',
+			selected: true
+		}],
+		loadMode: 'paginate-sides',
+		loadButtonSize: 'small',
+		hideHeader: true,
+		hideNavigation: true,
+		hideComments: true,
+		maxResults: 4,
+		tabStyle: 'wire',
+		youmaxBackgroundColor: '#ffffff',
+		maxContainerWidth: 1120,
+		fourColumnThumbnailWidth: '21.429%',
+		fourColumnThumbnailLeftRightMargin: '1.78%',
+		videoProtocol: 'https:'
+	});
+};
+
+/*
+ * Window related events
+ */
+$win
+	.on('load', function() {
+		// Fix list links dashes
+		if ($('.list-links').length) {
+			var offset = $('.list-links li:first-child').offset().top; 
+
+			$('.list-links li').each(function() {
+				var $li           = $(this);
+				var elementOffset = $li.offset().top;
+
+				if (elementOffset !== offset) {
+					$li.addClass('is-newline');
+
+					offset = elementOffset;
+				}
+			});
+		}
+
+		// Slow down video
+		if ($('.js-video').length) {
+			$('.js-video')
+				.get(0)
+					.playbackRate = .7;
+		}
+
+		// Parallax tour eiffel
+		if (!('ontouchstart' in window)) {
+			skrollr.init();
+		}
+
+		// Temoignages slider
+		if ($('.slider__slides').length) {
+	 		$('.slider__slides').carouFredSel({
+	 			responsive: true,
+				items: 1,
+				swipe: {
+					onTouch: true,
+					onMouse: false
+				},
+				auto: {
+					play           : true,
+					timeoutDuration: 5000
+				},
+				prev: {
+					button: '.slider__prev'
+				},
+				next: {
+					button: '.slider__next'
+				},
+				infinite: true
+			});
+		}
+	}).on('load scroll', function() {
+		var winST = $win.scrollTop();
+
+	 	// Sticky header
+		$header.toggleClass('is-fixed', winST > 350);
+
+		// Sticky socials
+		$socials.toggleClass('sticky-visible', winST > $('#zone2').offset().top);
+
+		// Animated elements
+		$animate.each(function(){
+			var $this  = $(this);
+			var offset = $this.offset().top;
+
+			if (winST + ($win.outerHeight() * 0.8) > offset) {
+				$this.addClass('animated');
+			}
+		});
+
+		if (winST + $win.outerHeight() > $('.site-footer').offset().top) {
+			$animate.addClass('animated');
+		}
+	});
+
+/*
+ * Google maps
+ */
+window.initMap = function() {
+	var $map = $('.map');
+	var lat  = $map.data('lat');
+	var lng  = $map.data('lng');
+
+	new google.maps.Map($map.get(0), {
+		center			: {
+			lat: lat,
+			lng: lng
+		},
+		zoom   			: 13,
+		disableDefaultUI: true,
+		styles 		    : [{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}]
+	});
+};
