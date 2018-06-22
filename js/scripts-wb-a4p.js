050c75f21a86d4dbbf183877ad2ed199f20a0139
d-willb <39524812+d-willb@users.noreply.github.com>
2018-06-22 18:23:30 +0200

    Update scripts-wb-a4p.js

--- js/scripts-wb-a4p.js
+++ js/scripts-wb-a4p.js
@@ -214,20 +214,39 @@ $win
 	});
 
 /*
+ * Pagination gla items
+ */
+if ($('.cxp-pagination').length) {
+	var observer = new MutationObserver(function() {
+		if (!$('.cxp-pagination .container').length) {
+			$('.cxp-pagination .la-item-img').wrap('<div class="container"></div>');
+		}
+	});
+
+	observer.observe($('.cxp-pagination').get(0), {
+		childList: true,
+		subtree: true
+	});
+}
+
+/*
  * Google maps
  */
 window.initMap = function() {
 	var $map = $('.map');
-	var lat  = $map.data('lat');
-	var lng  = $map.data('lng');
 
-	new google.maps.Map($map.get(0), {
-		center			: {
-			lat: lat,
-			lng: lng
-		},
-		zoom   			: 13,
-		disableDefaultUI: true,
-		styles 		    : [{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}]
-	});
+	if ($map.length) {
+		var lat = $map.data('lat');
+		var lng = $map.data('lng');
+
+		new google.maps.Map($map.get(0), {
+			center			: {
+				lat: lat,
+				lng: lng
+			},
+			zoom   			: 13,
+			disableDefaultUI: true,
+			styles 		    : [{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}]
+		});
+	}
 };
